c998a7c288737d56a7644754ff8da73576629c22
Svetlana <podsvetser@yandex.ru>
2022-03-06 02:01:48 +0300

    Initial commit

---
+++ ConsoleApplication1/FileClass.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class FileClass
+    {
+        public delegate void OnListChanged(); //делегат
+        public List<string> list; //список со строками
+        private string fileName; //имя файла
+        public OnListChanged listChanged; // делегатная переменная
+
+        public FileClass(string fileName) //конструктор - заполняет список из файла
+        {
+            this.fileName = fileName;
+            list = File.ReadAllLines(fileName).ToList();
+        }
+
+        public void Add(string newString)
+        {
+            list.Add(newString); //добавляем в список
+            if (listChanged != null) listChanged(); //вызываем обработчик изменения списка
+            File.WriteAllLines(fileName, list); //сохраняем файл
+        }
+
+        public void Remove(int index)
+        {
+            list.RemoveAt(index); //удаляем из списка
+            if (listChanged != null) listChanged(); //вызываем обработчик изменения списка
+            File.WriteAllLines(fileName, list);  //сохраняем файл
+        }
+
+        public void Search(String str)
+        {
+            bool found = false;
+
+            using (StreamReader reader = new StreamReader("data.txt"))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (line.IndexOf(str) > -1)
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine("Found: {0}", found);
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static public int DisplayMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Главное меню");
+            Console.WriteLine();
+            Console.WriteLine("1. Добавить новую ИП или ЮЛ");
+            Console.WriteLine("2. Найти ИП или ЮЛ");
+            Console.WriteLine("3. Список задач, встреч и др.");
+            Console.WriteLine("0. Выход");
+            Console.WriteLine();
+            Console.Write("Введите число и нажмите клавишу Enter: ");
+            var result = Console.ReadLine();
+            return Convert.ToInt32(result);// в int 
+        }
+        static public int DisplayMenuOrganizer()
+        {
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("1. Добавить мероприятие");
+            Console.WriteLine("2. Найти мероприятие");
+            Console.WriteLine("3. Назад");
+            Console.WriteLine();
+            Console.Write("Введите число и нажмите клавишу Enter: ");
+            var result = Console.ReadLine();
+            return Convert.ToInt32(result);// в int 
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                int userInput = 0;
+                do
+                {
+                    userInput = DisplayMenu();
+
+                    FileClass data = new FileClass("data.txt");
+
+                    switch (userInput)
+                    {
+                        case 1:
+                            Console.WriteLine("Вввод данных\nНазвание\tАдрес\tТелефон");
+                            data.Add(Console.ReadLine());
+                            Console.WriteLine("Успешно добавлены!\nДля продолжения нажмите Enter!");
+                            Console.ReadLine();
+                            break;
+                        case 2:
+                            Console.Write("Введите номер телефона: ");
+                            data.Search(Console.ReadLine());
+                            Console.WriteLine("Для продолжения нажмите Enter!");
+                            Console.ReadLine();
+                            break;
+                        //////////////////////////////////////////////////////////////
+                        case 3:
+                            do
+                            {
+                                userInput = DisplayMenuOrganizer();
+                                switch (userInput)
+                                {
+                                    case 1:
+                                        Console.WriteLine("Вввод данных\nНаименование\tДата начала\tДата окончания\tТелефон организации");
+                                        data.Add(Console.ReadLine());
+                                        Console.WriteLine("Для продолжения нажмите Enter!");
+                                        Console.ReadLine();
+                                        break;
+                                    case 2:
+                                        Console.Write("Введите номер телефона организации: ");
+                                        String phone = Console.ReadLine();
+                                        data.Search(phone);
+                                        Console.WriteLine("Для продолжения нажмите Enter!");
+                                        Console.ReadLine();
+                                        break;
+                                    case 3:
+                                        userInput = DisplayMenu();
+                                        break;
+                                    default:
+                                        Console.WriteLine("Не существующее значение!");
+                                        userInput = DisplayMenuOrganizer();
+                                        break;
+                                }
+
+                            } while (userInput != 0);
+                            //////////////////////////////////////////////////////////////
+                            break;
+                        default:
+                            Console.WriteLine("Не существующее значение!");
+                            userInput = DisplayMenu();
+                            break;
+                    }
+
+                } while (userInput != 0);
+
+
+                //System.Threading.Thread.Sleep(1000);//Пауза
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+    }
+}
